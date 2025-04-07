3d56d43af5da8d97fc2a56de13c29f222b05ec55
patilsign <patilsign108@gmail.com>
2025-04-07 12:47:15 +0530

    created header-section for app

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import React from "react";
+import classes from "./main-navigation.module.css";
+import Logo from "./logo";
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        <Logo />
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">All Posts</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import { Fragment } from "react";
-import Banner from "../components/home-page/banner";
 import FeaturedPosts from "../components/home-page/featured-post";
+import Hero from "../components/home-page/hero";
 
 export default function Home() {
   return (
     <Fragment>
-      <Banner />
+      <Hero />
       <FeaturedPosts />
     </Fragment>
   );
