cff57cbce2431d9eaa6c49aa371a8755ab8c7288
patilsign <patilsign108@gmail.com>
2025-04-07 13:37:34 +0530

    Created HomePage Section

--- components/home-page/featured-post.js
+++ components/home-page/featured-post.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import classes from "./featured-post.module.css";
 
 const FeaturedPosts = () => {
   return (
-    <div>featured-post</div>
-  )
-}
+    <section className="latest">
+      <h2>Featured Posts</h2>
+      <div>...</div>
+    </section>
+  );
+};
 
-export default FeaturedPosts
+export default FeaturedPosts;
--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import Image from "next/image";
+import React from "react";
+import classes from "./hero.module.css";
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/sign.png"
+          alt="its authors image"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Sign's Blog</h1>
+      <p>
+        I am learning next JS with practice, developing my own blog App is the
+        perfect example of learn with practice.
+      </p>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
